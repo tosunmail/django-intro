0b35e435e49607d4690e3bf6718016de59e35b31
tarik <tosunmaildir@gmail.com>
2023-05-06 11:55:54 +0200

    router

---
+++ models/fscohort/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+
+def fscohort(request):
+    return HttpResponse('''
+    <h2>
+    Welcome to FsCohort
+    </h2>
+    ''')
+
+def goodbye(request):
+    return HttpResponse('''
+    <h2>
+    goodbye
+    </h2>
+    ''')
