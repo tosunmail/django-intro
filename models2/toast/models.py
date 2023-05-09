8f141bc0d27e910164f0051aaa005ca9e9cc9b0e
tarik <tosunmaildir@gmail.com>
2023-05-09 14:20:24 +0200

    models 2

---
+++ models2/toast/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# https://docs.djangoproject.com/en/4.2/topics/db/models/
+class Student(models.Model):
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#field-types
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField('--', null=True , blank=True)
+    number = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='images/' , default='',null=True , blank=True)
+    created = models.DateTimeField(auto_now_add=True) # Kayıt yapıldığı anki zamanı otomatik yaz.
+    updated = models.DateTimeField(auto_now=True) # Kayıt güncellendiği anki zamanı otomatik yaz.
+
+    def __str__(self): # Kayıt yazdır.
+        return f'{self.first_name} {self.last_name} # {self.number}'
+    
+    class Meta: # Default özellikleri değiştir.
+        # https://docs.djangoproject.com/en/4.2/ref/models/options/
+        verbose_name = 'Student'
+      
+        ordering = ["-first_name"] # Ters sıralam için (DESC) sutun isminin başına - konur.
