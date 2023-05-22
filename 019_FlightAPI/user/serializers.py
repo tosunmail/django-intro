8a485f0bd2ebb11b454c25866e3d2ae0ce9995d2
tarik <tosunmaildir@gmail.com>
2023-05-22 20:58:04 +0200

    email validator

---
+++ 019_FlightAPI/user/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
+class UserSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(
+        required=True,
+        validators = [UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+       
+        exclude = [
+            # "password",
+            "last_login",
+            "date_joined",
+            "groups",
+            "user_permissions",
+        ]
+
+        
+
+    def validate(self, attrs):
+        from django.contrib.auth.password_validation import validate_password # doğrulama fonksiyonu
+        from django.contrib.auth.hashers import make_password # şifreleme fonksiyonu
+        password = attrs['password'] # Password al.
+        validate_password(password) # Validation'dan geçir.
+        attrs.update(
+            {
+                'password': make_password(password) # Password şifrele ve güncelle.
+            }
+        )
+        return super().validate(attrs) # Orjinal methodu çalıştır.
