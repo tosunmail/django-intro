f4b0bc1afdb76541d5f0f3187d9d5de735ea5356
tarik <tosunmaildir@gmail.com>
2023-05-29 19:48:52 +0200

    department

--- personnel-API/personnel/models.py
+++ personnel-API/personnel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=32)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+
